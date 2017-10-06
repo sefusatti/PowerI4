5f80d692e80051a17e56ae6946a27a2d526058e2
emi <emiliano.sefusatti@gmail.com>
2017-10-06 15:31:25 +0200

    nothing

--- PowerI4.f90
+++ PowerI4.f90
@@ -2,7 +2,7 @@
 
 !{ Developers = Emiliano Sefusatti, Martin Crocce !}
 !{ Based on previous codes by R. Scoccimarro and H. Couchman - !}
-!{ Version    = beta       !} 
+!{ Version    = beta       !}
 !{ Time-stamp = Oct - 2017 !}
 !{ 2017-10-04
 !{- FFT in place for interlacing
@@ -11,58 +11,55 @@
 !{- Normalization by 1/kF3 in fcomb
 
 !{ Subroutines in this file:
-!{ input_parameters_file     
+!{ input_parameters_file
 !{ print_initial_log
-!{ input_parameters_file           
+!{ input_parameters_file
 !{ output_real_space_density
 !{ output_fourier_space_density
 !{ fcomb
-!{ measure_pk 
+!{ measure_pk
 
-!{ Module parbox      
+!{ Module parbox
 
-!{ Module grid 
+!{ Module grid
 
 !{ Module input_catalog:
-!{ input_catalog_xyz     
-!{ input_catalog_gadget      
+!{ input_catalog_xyz
+!{ input_catalog_gadget
 
 !{ Module interpolate_particles:
 !{ assign
 !{ assign_direct
-!{ assign_double_grid      
+!{ assign_double_grid
 !{ assign_single_grid
 !{ assign_index
 !{ assign_weights_2
 !{ assign_weights_3
 !{ assign_weights_4
 
-
-! {ciccia
-      
     use input_catalog
-    use parbox 
+    use parbox
     use grid
-    use, intrinsic   :: iso_c_binding ! for FFTW3       
+    use, intrinsic   :: iso_c_binding ! for FFTW3
     implicit         none
     integer(kind=8)  :: planf
-      
+
     include 'fftw3.f'  ! FFTW3 header
 
 !{  input parameters from external file  !}
 
       call input_parameters_file
-      call print_initial_log 
- 
+      call print_initial_log
+
 !{  Allocating grids !}
 
       write(*,'(A)') ' allocating grid'
       if (nintp.ne.0) then
          if (interlacing) then
             ! allocate a single complex array
-            allocate(dcl(Nx,Ny,Nz)) 
+            allocate(dcl(Nx,Ny,Nz))
          else if (.not. interlacing) then
-            ! allocate a complex and real arrays 
+            ! allocate a complex and real arrays
             allocate(dcl(Nx/2+1,Ny,Nz),dtl(Nx,Ny,Nz))
          endif
          rupr=(1.d0-tiny(1.d0))  !make sure there is no problem later with interp.
@@ -73,7 +70,7 @@
 
 !{ Initializing grids!}
 
-      if (.not. interlacing) dtl=0.d0 
+      if (.not. interlacing) dtl=0.d0
       dcl=dcmplx(0.d0,0.d0)
 
 !{ Reading catalogs and assigning particles !}
@@ -81,41 +78,41 @@
       write(*,'(A)') ' doing particle assignment'
 
       call input_chosen_catalog(infiletype)
-      
+
       write(*,'(A,I12)') ' total number of particles = ',Nptot
 
 !{ Box & grid parameters !}
-      
+
       kFx=twopi/Rx             ! fundamental frequencies
       kFy=twopi/Ry
       kFz=twopi/Rz
       kF3=kFx*kFy*kFz
 
-      kNx=kFx*dble(Nx)/2.d0    ! Nyquist frequencies 
-      kNy=kFy*dble(Ny)/2.d0   
-      kNz=kFz*dble(Nz)/2.d0   
-       
+      kNx=kFx*dble(Nx)/2.d0    ! Nyquist frequencies
+      kNy=kFy*dble(Ny)/2.d0
+      kNz=kFz*dble(Nz)/2.d0
+
        write(*,'(A,3F12.6)') ' fundamental frequencies = ',kFx,kFy,kFz
        write(*,'(A,3F12.6)') ' Nyquist frequencies = ',kNx,kNy,kNz
 
 !{ Normalization !}
 
       if (nintp.eq.0) then
-         dcl=dcl/dble(Nptot)/kF3  
+         dcl=dcl/dble(Nptot)/kF3
       else if (interlacing) then
-         dcl=dcl/dble(Nptot) 
+         dcl=dcl/dble(Nptot)
       else
-         dtl=dtl/dble(Nptot) 
+         dtl=dtl/dble(Nptot)
       endif
-      
+
 !{ FFT !}
 
-       if (nintp.ne.0) then 
+       if (nintp.ne.0) then
 
-          write(*,'(A)') ' doing FFT' 
-       
+          write(*,'(A)') ' doing FFT'
+
           if (interlacing) then
-             ! FFTW complex to complex in place 
+             ! FFTW complex to complex in place
              call dfftw_plan_dft_3d(planf,Nx,Ny,Nz,dcl,dcl,FFTW_ESTIMATE)
              call dfftw_execute(planf)
           else
@@ -129,30 +126,30 @@
           call fcomb
 
        endif
-       
+
        call output_fourier_space_density ! only if required
 
        call measure_pk
 
        write(*,'(A)') ' all done'
        write(*,*)
-       
+
     stop
     end program PowerI4
 
-    
-    
+
+
 ! *************************************************************************************
 ! *** Input parameters ****************************************************************
 
-    
+
     subroutine input_parameters_file
 
     use parbox
     use grid, only: Nx,Ny,Nz
     implicit none
     character(5) :: car
-       
+
       read(*,*) ! input file name
       read(*,*) infile
       read(*,*) ! type of input file
@@ -175,9 +172,9 @@
       read(*,*) Rx
       read(*,*) Ry
       read(*,*) Rz
-      read(*,*) ! center of the first bin in units of the fundamental frequency 
+      read(*,*) ! center of the first bin in units of the fundamental frequency
       read(*,*) rkF
-      read(*,*) ! size of the bin in units of the fundamental frequency 
+      read(*,*) ! size of the bin in units of the fundamental frequency
       read(*,*) rdk
       read(*,*) ! measure multiples
       read(*,*) iRSD
@@ -186,16 +183,16 @@
       ! ioutput = 1 / 2 : real-space / Fourier-space density
       ! ioutput = -1/-2 : real-space / Fourier-space density, stop after output
       read(*,*) ! output density file
-      read(*,*) file_density 
-       
+      read(*,*) file_density
+
     end subroutine input_parameters_file
 
-    
-    
+
+
 ! *************************************************************************************
 ! *** Print inputs to screen **********************************************************
 
-    
+
     subroutine print_initial_log
 
     use parbox
@@ -226,11 +223,11 @@
     end subroutine print_initial_log
 
 
-        
+
 ! ************************************************************************************
 ! *** fcomb **************************************************************************
 
-    
+
     subroutine fcomb
 
     use parbox, only : nintp, interlacing, twopi, kF3
@@ -245,7 +242,7 @@
        Nnyqx=Nx/2+1
        tpiNx=twopi/dble(Nx)
        Nnyqy=Ny/2+1
-       tpiNy=twopi/dble(Ny) 
+       tpiNy=twopi/dble(Ny)
        Nnyqz=Nz/2+1
        tpiNz=twopi/dble(Nz)
 
@@ -268,7 +265,7 @@
        if (interlacing) then
 
           cf=1.d0/(4.d0*kF3)
-          
+
           piNx=-tpiNx/2.d0
           recx=dcmplx(dcos(piNx),dsin(piNx))
           piNy=-tpiNy/2.d0
@@ -283,29 +280,29 @@
           do iz=1,Nnyqz
              icz=mod(Nz-iz+1,Nz)+1
              Wkz=tWkz(iz)
-             
+
              yrec=c1
              do iy=1,Nnyqy
                 icy=mod(Ny-iy+1,Ny)+1
                 Wky=tWky(iy)
-                
+
                 xrec=c1
                 do ix=1,Nnyqx
                    icx=mod(Nx-ix+1,Nx)+1
                    Wkx=tWkx(ix)
 
                    cfac=cf/(Wkx*Wky*Wkz)
-                   
+
                    cma=ci*xrec*yrec*zrec
                    cmb=ci*xrec*yrec*dconjg(zrec)
                    cmc=ci*xrec*dconjg(yrec)*zrec
                    cmd=ci*xrec*dconjg(yrec*zrec)
-                   
+
                    c000=dcl(ix,iy ,iz )*(c1-cma)+dconjg(dcl(icx,icy,icz))*(c1+cma)
                    c001=dcl(ix,iy ,icz)*(c1-cmb)+dconjg(dcl(icx,icy,iz ))*(c1+cmb)
                    c010=dcl(ix,icy,iz )*(c1-cmc)+dconjg(dcl(icx,iy ,icz))*(c1+cmc)
                    c011=dcl(ix,icy,icz)*(c1-cmd)+dconjg(dcl(icx,iy ,iz ))*(c1+cmd)
-                 
+
                    dcl(ix,iy ,iz )=c000*cfac
                    dcl(ix,iy ,icz)=c001*cfac
                    dcl(ix,icy,iz )=c010*cfac
@@ -314,7 +311,7 @@
                    dcl(icx,iy ,icz)=dconjg(dcl(ix,icy,iz ))
                    dcl(icx,icy,iz )=dconjg(dcl(ix,iy ,icz))
                    dcl(icx,icy,icz)=dconjg(dcl(ix,iy ,iz ))
-                 
+
                    xrec=xrec*recx
                 enddo
                 yrec=yrec*recy
@@ -329,15 +326,15 @@
         do iz=1,Nnyqz
            icz=mod(Nz-iz+1,Nz)+1
            Wkz=tWkz(iz)
-              
+
            do iy=1,Nnyqy
               icy=mod(Ny-iy+1,Ny)+1
               Wky=tWky(iy)
-              
+
               do ix=1,Nnyqx
                  Wkx=tWkx(ix)
-                 
-                 cfac=cf/(Wkx*Wky*Wkz)                 
+
+                 cfac=cf/(Wkx*Wky*Wkz)
                  dcl(ix,iy ,iz )=dcl(ix,iy ,iz )*cfac
                  if(iz.ne.icz) dcl(ix,iy,icz)=dcl(ix,iy,icz)*cfac
                  if(iy.ne.icy) dcl(ix,icy,iz)=dcl(ix,icy,iz)*cfac
@@ -345,26 +342,26 @@
 
               enddo
            enddo
-        enddo        
+        enddo
 
      endif
-         
+
     end subroutine fcomb
 
 
-    
+
 ! ***********************************************************************************
 ! *** Output densities  *************************************************************
 
-  
-    
+
+
     subroutine output_fourier_space_density
-      
+
       use parbox, only : ioutput,file_density,Nptot,kFx,kFy,kFz,kNx
       use grid, only   : Nx,Ny,Nz,dcl
       implicit none
       integer :: ix,iy,iz
-      
+
       if (abs(ioutput).eq.1) then
          open(unit=4,file=file_density,status='unknown',form='unformatted')
          write(4) Nx,Ny,Nz,dble(Nptot),kFx,kFy,kFz,kNx
@@ -377,15 +374,15 @@
          endif
       endif
 
-      
+
     end subroutine output_fourier_space_density
 
 
-        
-! ***********************************************************************************
-! *** Measure and output P(k)  ******************************************************    
 
-    
+! ***********************************************************************************
+! *** Measure and output P(k)  ******************************************************
+
+
     subroutine measure_pk
 
       use parbox
@@ -396,36 +393,36 @@
       real (kind=8) :: k,kFmin,rk,pSN,kNmax,dk
       real (kind=8) :: Le2, Le4, sp, sit1, cot1, rkx, rky, rkz, pk, coga, cp, cc
       real (kind=8) :: thetaobs, cthetaobs, sthetaobs, phiobs
-    complex (kind=8) :: ct  
+    complex (kind=8) :: ct
 
       write(*,'(A)') ' measuring power spectrum'
-       
+
       kFmin=min(kFx,min(kFy,kFz))
-      kNmax=max(kNx,max(kNy,kNz)) 
+      kNmax=max(kNx,max(kNy,kNz))
 
       ! bin size is determined based on the smallest fundamental frequency
-      ! while the number of bins depends on the largest Nyquist frequency 
+      ! while the number of bins depends on the largest Nyquist frequency
 
       dk=rdk*kFmin   ! size of the wavenumbers bin
       Nbins=nint(kNmax/dk)
       write(*,'(A,I5)') ' number of bins = ', Nbins
-       
+
       pSN=1.d0/(kF3*dble(Nptot))     ! shot-noise contribution for Poisson distribution
-       
+
       allocate (avgk(Nbins),avgP(Nbins),co(Nbins))
       avgk=0.d0
       avgP=0.d0
       co=0.d0
 
-      thetaobs=0.d0          
+      thetaobs=0.d0
       sthetaobs=dsin(thetaobs)
       cthetaobs=dcos(thetaobs)
-      phiobs=0.d0 
+      phiobs=0.d0
       if (iRSD.eq.0) then   ! no multiples
-         thetaobs=0.d0          
-         phiobs=0.d0 
+         thetaobs=0.d0
+         phiobs=0.d0
       else                  ! multiples
-         allocate (avgP2(Nbins),avgP4(Nbins))  
+         allocate (avgP2(Nbins),avgP4(Nbins))
          avgP2=0.d0
          avgP4=0.d0
          if (iRSD.eq.1) then
@@ -458,10 +455,10 @@
          rkx=kFx*float(ikx)
 
          if (ix.ne.1 .or. (ix.eq.1 .and. iky.gt.0) .or. (ix.eq.1 .and. iky.eq.0 .and. ikz.gt.0)) then
-   
+
          rk=dsqrt(rkx**2+rky**2+rkz**2)
          imk=nint(rk/dk-rkF/rdk-tiny(kFmin))+1
-                
+
            if (imk.le.Nbins .and. imk.gt.0) then
 
              co(imk)=co(imk)+1.d0
@@ -473,7 +470,7 @@
              endif
              pk=(cdabs(ct))**2
              avgP(imk)=avgP(imk)+pk
-                   
+
              if (iRSD.ne.0) then
              cot1=rkz/rk
              sit1=dsqrt(1.d0 - cot1*cot1)
@@ -517,10 +514,4 @@
       enddo
       close(4)
 
-    end subroutine measure_pk    
-
-
-
-
-
-
+    end subroutine measure_pk
